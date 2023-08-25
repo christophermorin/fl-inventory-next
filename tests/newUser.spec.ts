5dd204b5c43601a301ef82f1bd88cdc88c5ee539
Chris Morin <longhumansbiz@gmail.com>
2023-08-25 19:40:01 -0300

    test: newUser spec

--- tests/newUser.spec.ts
+++ tests/newUser.spec.ts
@@ -17,11 +17,11 @@ test.describe("New user workflow", () => {
     // User is redirected to Sign In page after registering
     await expect(page).toHaveTitle(/Frontline - Sign In/);
   });
-  test("sign in user", async ({ page }) => {
-    await page.goto("/auth/signin");
-    await page.getByPlaceholder("Email").fill("newUser@mail.com");
-    await page.getByPlaceholder("Password").fill("pAssword123!");
-    await page.getByRole("button", { name: "Sign In" }).click();
-    await expect(page).toHaveTitle(/Frontline - Organization Setup/);
-  });
+  // test("sign in user", async ({ page }) => {
+  //   await page.goto("/auth/signin");
+  //   await page.getByPlaceholder("Email").fill("newUser@mail.com");
+  //   await page.getByPlaceholder("Password").fill("pAssword123!");
+  //   await page.getByRole("button", { name: "Sign In" }).click();
+  //   await expect(page).toHaveTitle(/Frontline - Organization Setup/);
+  // });
 });
