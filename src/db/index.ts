531584a99f290a3f546d0e1e222532aa5c4d0453
Chris Morin <longhumansbiz@gmail.com>
2023-08-25 19:51:51 -0300

    test: added next api url env

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,11 +2,11 @@ import { Pool, QueryResult } from "pg";
 import type { QueryConfig } from "@/types/main";
 
 const config = {
-  user: process.env.POSTGRES_USER!,
+  user: process.env.POSTGRES_USER! || "postgres",
   host: process.env.POSTGRES_HOST!,
-  database: process.env.POSTGRES_NAME!,
+  database: process.env.POSTGRES_NAME! || "postgres",
   password: process.env.POSTGRES_PASSWORD!,
-  port: Number(process.env.POSTGRES_PORT)!,
+  port: Number(process.env.POSTGRES_PORT)! || 5432,
   // ssl: true,
   max: 20, // set pool max size to 20
   idleTimeoutMillis: 5000, // close idle clients after 1 second
