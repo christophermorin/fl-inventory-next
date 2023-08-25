b5474bb839e7427870f39ea377e7814113fc8569
Chris Morin <longhumansbiz@gmail.com>
2023-08-25 20:14:14 -0300

    test: one more try in dev

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,11 +2,11 @@ import { Pool, QueryResult } from "pg";
 import type { QueryConfig } from "@/types/main";
 
 const config = {
-  user: process.env.POSTGRES_USER! || "postgres",
+  user: process.env.POSTGRES_USER!,
   host: process.env.POSTGRES_HOST!,
-  database: process.env.POSTGRES_NAME! || "postgres",
+  database: process.env.POSTGRES_NAME!,
   password: process.env.POSTGRES_PASSWORD!,
-  port: Number(process.env.POSTGRES_PORT)! || 5432,
+  port: Number(process.env.POSTGRES_PORT)!,
   // ssl: true,
   max: 20, // set pool max size to 20
   idleTimeoutMillis: 5000, // close idle clients after 1 second
